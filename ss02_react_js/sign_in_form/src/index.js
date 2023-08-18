3a65ac120f13e9f62a49f593718b03b0d18b230b
thanhtruong0123 <thanhtruong0123@gmail.com>
2023-08-18 18:59:05 +0700

    REACT JS

--- ss02_react_js/sign_in_form/src/index.js
+++ ss02_react_js/sign_in_form/src/index.js
@@ -8,7 +8,6 @@ import "bootstrap/dist/css/bootstrap.css";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <h1>Students: </h1>
     <App />
   </React.StrictMode>
 );
